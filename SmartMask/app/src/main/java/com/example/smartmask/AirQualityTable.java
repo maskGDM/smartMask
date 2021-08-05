815f4c8f8b4f9446f39d8f228dff41e67717c2ec
DAYANNA LIZZET VELEZ ARAUJO <dayanna.velez2017@uteq.edu.ec>
2021-08-04 20:45:35 -0500

    DatePickerDialog

--- SmartMask/app/src/main/java/com/example/smartmask/AirQualityTable.java
+++ SmartMask/app/src/main/java/com/example/smartmask/AirQualityTable.java
@@ -1,10 +1,16 @@
 package com.example.smartmask;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TableLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -18,12 +24,16 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.json.JSONException;
+
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
 
 public class AirQualityTable extends AppCompatActivity {
+    private DatePickerDialog datePickerDialog,dateFinish;
+    private TextView txtFechaInicio,txtFechaFin;
+
+
     String jsonDataUser = "{}";
     RequestQueue requestQueue;
     String URL = "https://aplicaciones.uteq.edu.ec/smartmask/webapis/";
@@ -44,7 +54,81 @@ public class AirQualityTable extends AppCompatActivity {
         Log.i("Logs", jsonLogin);
         stringRequestVolley(jsonLogin);
 
+        initDatePicker();
+        txtFechaInicio=findViewById(R.id.txtFechaInicio);
+        txtFechaFin=findViewById(R.id.txtFechaFin);
+
+        txtFechaInicio.setText(getTodayDate());
+        
 }
+    private String getTodayDate(){
+        Calendar cal= Calendar.getInstance();
+        int year=cal.get(Calendar.YEAR);
+        int month=cal.get(Calendar.MONTH);
+        int day=cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day,month,year);
+    }
+    private void initDatePicker(){
+        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month=month+1;
+                String date=makeDateString(day,month,year);
+                String finaldate=makeDateStringFinish(day,month,year);
+                //txtFechaInicio.setText(date);
+                //txtFechaFin.setText(finaldate);
+            }
+        };
+        Calendar cal= Calendar.getInstance();
+        int year=cal.get(Calendar.YEAR);
+        int month=cal.get(Calendar.MONTH);
+        int day=cal.get(Calendar.DAY_OF_MONTH);
+        int style= AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog=new DatePickerDialog(this,style,dateSetListener,year,month,day);
+       dateFinish=new DatePickerDialog(this,style,dateSetListener,year,month,day);
+    }
+    private String makeDateString(int day, int month, int year){
+        return getMonthFormat(month)+ " " + day + " " + year;
+    }
+    private String makeDateStringFinish(int day, int month, int year){
+        return getMonthFormat(month)+ " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+        if (month==1){
+            return "Jan";
+        }if (month==2){
+            return "Feb";
+        }if (month==3){
+            return "Mar";
+        }if (month==4){
+            return "Apr";
+        }if (month==5){
+            return "May";
+        }if (month==6){
+            return "Jun";
+        }if (month==7){
+            return "Jul";
+        }if (month==8){
+            return "Aug";
+        }if (month==9){
+            return "Sep";
+        }if (month==10){
+            return "Oct";
+        }if (month==11){
+            return "Nov";
+        }if (month==12){
+            return "Dec";
+        }
+        return "Jan";
+    }
+    public void openDatePicker(View view){
+        datePickerDialog.show();
+    }
+    public void finishDate(View view){
+        dateFinish.show();
+    }
 
     private void stringRequestVolley(String json) {
         StringRequest request = new StringRequest(Request.Method.POST, URL + "maskapis/maskRecords", new com.android.volley.Response.Listener<String>() {
