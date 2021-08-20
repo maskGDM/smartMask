9730d1395ba2c38573e380958232558cbd28eded
jorgmoli97 <jorgmoli97@users.noreply.github.com>
2021-08-20 01:22:28 -0500

    activate mask
    activate mask

--- SmartMask/app/src/main/java/com/example/smartmask/ConfigureValues.java
+++ SmartMask/app/src/main/java/com/example/smartmask/ConfigureValues.java
@@ -11,7 +11,25 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConfigureValues extends AppCompatActivity {
+
+    private String URL = "https://aplicaciones.uteq.edu.ec/smartmask/webapis/";
+    private RequestQueue requestQueue;
+
     private EditText Dioximin, Dioximax, Monomin, Monomax, Ammomin, Ammomax,Butanomin, Butanomax;
     private Button btnsave;
     //session
@@ -44,6 +62,7 @@ public class ConfigureValues extends AppCompatActivity {
                                 "    \"Butanomax\":\"" + Butanomax.getText()+"\"\n" +
                                 "}";
                         Log.i("Logs", jsondata);
+                      //  configurevaluesPOST(jsondata);
                     }else{
                         Toast.makeText(ConfigureValues.this, "Empty fields", Toast.LENGTH_LONG).show();
                     }
@@ -84,5 +103,71 @@ public class ConfigureValues extends AppCompatActivity {
         email= preferences.getString("email",null);
         imguser= preferences.getString("imguser",null);
         birthdaydate= preferences.getString("birthdaydate",null);
+    }
+    public static String fixEncoding(String response) {
+        try {
+            byte[] u = response.toString().getBytes(
+                    "ISO-8859-1");
+            response = new String(u, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return response;
+    }
+
+    private void configurevaluesPOST(String datajson){
+
+        //Obtención de datos del web service utilzando Volley
+        // requestQueue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(
+                Request.Method.POST,URL+"",
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        int size = response.length();
+                        response = fixEncoding(response);
+                        JSONObject json_transform = null;
+                        try {
+                            if (size > 0)
+                            {
+                                json_transform = new JSONObject(response);
+                              //  Toast.makeText(ConfigureValues.this, json_transform.getString("information"), Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", String.valueOf(error));
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json; charset=utf-8");
+                params.put("Accept", "application/json");
+                return params;
+            }
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return datajson == null ? "{}".getBytes("utf-8") : datajson.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+
+                    return null;
+                }
+            }
+        };
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(request);
+        } else {
+            requestQueue.add(request);
+        }
     }
 }
