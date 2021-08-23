cd32f5c57a2498e0b2e750c7f9817d1338a72e10
jorgmoli97 <jorgmoli97@users.noreply.github.com>
2021-08-22 23:25:17 -0500

    push
    push

--- SmartMask/app/src/main/java/com/example/smartmask/adapters/ListAdapterMask.java
+++ SmartMask/app/src/main/java/com/example/smartmask/adapters/ListAdapterMask.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -44,17 +45,20 @@ public class ListAdapterMask extends RecyclerView.Adapter<ListAdapterMask.ViewHo
     public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView imgfoto;
         TextView mask_code, dateadded;
+        Switch Accion;
 
         ViewHolder(View itemsView) {
             super(itemsView);
             imgfoto = itemView.findViewById(R.id.imgfoto);
             mask_code = itemView.findViewById(R.id.mask_code);
             dateadded = itemView.findViewById(R.id.dateadded);
+            Accion = (Switch) itemsView.findViewById(R.id.Accion);
         }
 
         void bindData(final MaskModel item){
             mask_code.setText(item.getMask_code());
             dateadded.setText(item.getDateadded());
+            Boolean switchState = Accion.isChecked();
             Glide.with(context).load("https://www.elmundo.cr/wp-content/uploads/2020/05/WhatsApp-Image-2020-05-20-at-13.19.19.jpeg").into(imgfoto);
         }
     }
