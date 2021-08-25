32d589419ae86b3ac9352e8c0414fdbc7dfeb299
geobricex <geovanny.brito2017@uteq.edu.ec>
2021-08-25 00:40:14 -0500

    Data

--- SmartMask-Web/src/main/java/Controller/DataCtrl.java
+++ SmartMask-Web/src/main/java/Controller/DataCtrl.java
@@ -6,9 +6,11 @@
 package Controller;
 
 import Config.WeEncoder;
+import DAOmodel.DataDAO;
 import DAOmodel.MaskDAO;
 import DAOmodel.RecordsDAO;
 import DAOmodel.UserDAO;
+import Model.Data;
 import Model.Mask;
 import Model.User;
 import Model.User_information;
@@ -18,26 +20,39 @@ import java.io.File;
  *
  * @author geova
  */
-public class MaskCtrl {
+public class DataCtrl {
 
-    MaskDAO maskDAO;
+    DataDAO dataDAO;
     WeEncoder weEncoder;
 
-    public MaskCtrl() {
-        maskDAO = new MaskDAO();
+    public DataCtrl() {
+        dataDAO = new DataDAO();
         weEncoder = new WeEncoder();
     }
 
-    public String[] saveMask(
-            String mask_code,
-            String user_informationid
+    public String[] saveData(
+            String user_informationid,
+            Double maxvalueco2,
+            Double minvalueco2,
+            Double maxvalueco,
+            Double minvalueco,
+            Double maxvaluec4h10,
+            Double mainvaluec4h10,
+            Double maxvaluenh3,
+            Double minvaluenh3
     ) {
-        Mask mask = new Mask();
-        mask.setMask_code(mask_code);
-        mask.setUser_informationid(user_informationid);
+        Data dataMask = new Data();
+        dataMask.setUser_informationid(user_informationid);
+        dataMask.setMaxvalueco2(maxvalueco2);
+        dataMask.setMinvalueco2(minvalueco2);
+        dataMask.setMaxvalueco(maxvalueco);
+        dataMask.setMinvalueco(minvalueco);
+        dataMask.setMaxvaluec4h10(maxvaluec4h10);
+        dataMask.setMainvaluec4h10(mainvaluec4h10);
+
         String status = "4", message = "Error al retornar datos", data = "[]";
 
-        String[] resp = maskDAO.saveMask(mask);
+        String[] resp = dataDAO.saveData(dataMask);
 
         if (resp[0].equals("2")) {
             status = resp[0];
@@ -50,42 +65,40 @@ public class MaskCtrl {
         return new String[]{status, message, data};
     }
 
-    public String[] selectMask(
+    public String[] selectData(
             String user_informationid
     ) {
-        Mask mask = new Mask();
-        mask.setUser_informationid(user_informationid);
+        Data dataMask = new Data();
+        dataMask.setUser_informationid(user_informationid);
         String status = "4", message = "Error al retornar datos", data = "[]";
 
-        String[] resp = maskDAO.selectMask(mask);
+        String[] resp = dataDAO.selectData("1", dataMask);
 
         if (resp[0].equals("2")) {
             status = resp[0];
             data = resp[1];
             message = "Los datos se han retornado correctamente";
-        } 
-        
-        return new String[]{status, message, data};
-    }
-
-    public String[] deleteMask(
-            String mask_code
-    ) {
-        Mask mask = new Mask();
-        mask.setMask_code(mask_code);
-        String status = "4", message = "Error al retornar datos", data = "[]";
-
-        String[] resp = maskDAO.deleteMask(mask);
-
-        if (resp[0].equals("2")) {
-            status = resp[0];
-            data = resp[1];
-            message = "Los datos se han retornado correctamente";
-        } else if (resp[0].equals("3")) {
-            status = resp[0];
-            message = "La mascarilla se encuentra ya en uso";
         }
+
         return new String[]{status, message, data};
     }
-    
+
+    public String[] restoreData(
+            String user_informationid
+    ) {
+        Data dataMask = new Data();
+        dataMask.setUser_informationid(user_informationid);
+        String status = "4", message = "Error al retornar datos", data = "[]";
+
+        String[] resp = dataDAO.restoreData(dataMask);
+
+        if (resp[0].equals("2")) {
+            status = resp[0];
+            data = resp[1];
+            message = "Los datos se han retornado correctamente";
+        }
+
+        return new String[]{status, message, data};
+    }
+
 }
--- SmartMask-Web/src/main/java/DAOmodel/DataDAO.java
+++ SmartMask-Web/src/main/java/DAOmodel/DataDAO.java
@@ -8,10 +8,12 @@ package DAOmodel;
 import Config.Conection;
 import Interfaces.DataCrud;
 import Model.Data;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * This DAO model class contains the attributes and methods of Data
+ *
  * @authors Brito,Solano,Velez
  * @version 1.0
  */
@@ -21,53 +23,93 @@ public class DataDAO implements DataCrud {
     Data categ = new Data();
 
     /**
-     * Method to obtain a json-chain with the information of a
-     * Data
+     * Method to obtain a json-chain with the information of a Data
+     *
      * @param data Receive the model of the data
      * @return Returns Data Information
      */
     public String jsonData(Data data) {
         return conex.getRecordsInJson("select * from data where user_informationid =" + data.getUser_informationid());
     }
+
     /**
      * Method to obtain a json-chain with the information of a Data
+     *
      * @return Returns default information
      */
-
     public String jsonData() {
         return conex.getRecordsInJson("select * from default_data order by defaultdataid desc limit 1;");
     }
-    
+
     /**
      * Method for aggregating data
+     *
      * @param data Receive the Data model
-     * @return Return a Boolean if the information was inserted 
+     * @return Return a Boolean if the information was inserted
      */
-
     @Override
     public boolean addData(Data data) {
-        return conex.modifyBD("select addData('"+data.getJson2Xml()+"')");
+        return conex.modifyBD("select addData('" + data.getJson2Xml() + "')");
     }
-    
+
     /**
-     * Method for editing data 
+     * Method for editing data
+     *
      * @param data Receive the Data model
      * @return Return a Boolean if the information was edit
      */
-
     @Override
     public boolean editData(Data data) {
         return false;
     }
-    
+
     /**
-     * Method for delete data 
+     * Method for delete data
+     *
      * @param data Receive the Data model
      * @return Return a Boolean if the information was delet
      */
-
     @Override
     public boolean deleteorresetData(Data data) {
         return false;
+    }
+
+    public String[] selectData(String type_query, Data data) {
+        String query = String.format("select * from listdefaultdata(%s, %s)", type_query, data.getUser_informationid());
+        System.out.println(query);
+        DefaultTableModel tab = conex.returnRecord(query);
+        if (tab.getRowCount() > 0) {
+            return new String[]{
+                tab.getValueAt(0, 0).toString(),
+                tab.getValueAt(0, 1).toString()};
+        } else {
+            return new String[]{"4", ""};
+        }
+    }
+
+    public String[] saveData(Data data) {
+        String query = String.format("select * from adddata('%s')", data.returnXml());
+        System.out.println(query);
+        DefaultTableModel tab = conex.returnRecord(query);
+        if (tab.getRowCount() > 0) {
+            return new String[]{
+                tab.getValueAt(0, 0).toString(),
+                tab.getValueAt(0, 1).toString()};
+        } else {
+            return new String[]{"4", ""};
+        }
+    }
+
+    public String[] restoreData(Data data) {
+        String query = String.format("select * from deletedata(%s)", data.getUser_informationid());
+        System.out.println(query);
+        DefaultTableModel tab = conex.returnRecord(query);
+        if (tab.getRowCount() > 0) {
+            return new String[]{
+                tab.getValueAt(0, 0).toString(),
+                tab.getValueAt(0, 1).toString()};
+        } else {
+            return new String[]{"4", ""};
+        }
     }
 }
